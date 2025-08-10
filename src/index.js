3b135806f9507bf38216464e6de9c6a1f718137a
Ebud <evanweagan@gmail.com>
2025-08-09 22:09:21 -0400

    updated submenu toggle

--- src/index.js
+++ src/index.js
@@ -558,14 +558,13 @@ function initMobileSubmenuToggle() {
     });
 
     // === Teardown: clean state on viewport change
-return () => {
-  toggleData.forEach(({ trigger, submenu, onClick }) => {
-    trigger.removeEventListener('click', onClick);
-    trigger.classList.remove('is-open');
-    // Remove the inline styles we added on mobile
-    gsap.set(submenu, { clearProps: 'height,overflow,display' }); // -> back to CSS (height:auto)
-  });
-};
+    return () => {
+      toggleData.forEach(({ trigger, submenu, onClick }) => {
+        trigger.removeEventListener('click', onClick);
+        trigger.classList.remove('is-open');
+        gsap.set(submenu, { height: "auto" });
+      });
+    };
   });
 }
 
